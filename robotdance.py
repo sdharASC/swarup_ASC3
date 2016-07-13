cb1a6ba0ad1b73556b3669f92419db8f0982e38b
Swarup Dhar <dragonqaz1@mail.com>
2016-07-13 10:33:16 -0400

    Created a more complicated version of the song

--- robotdance.py
+++ robotdance.py
@@ -1,22 +1,32 @@
+# Robot dance to Mr. C the slide man Cha Cha Song
+
 from Myro import *
 
 speed = 1
-seconds = 1
+seconds = .5
 
 i = 0
-while i < 3:
-    turnBy(90)
-    forward(speed, seconds)
-    backward(speed, seconds)
-    turnBy(180)
-    forward(speed, seconds)
-    i = i + 1
-    turnBy(360)
-    
-    
-i = 0
-while i < 3:
-    turnBy(90)
-    backward(speed, seconds)
-    forward(speed, seconds)
-    i = i + 1
+step = 0
+
+while step < 2:
+
+    while i < 3:
+        turnBy(90)
+        backward(speed, seconds + 1)
+        turnBy(20)
+        turnBy(-40)
+        backward(speed, seconds)
+        turnBy(180)
+        forward(speed, seconds)
+        i = i + 1
+        turnBy(360)
+        
+        
+    i = 0
+    while i < 3:
+        turnBy(-90)
+        backward(speed, seconds)
+        forward(speed, seconds)
+        i = i + 1
+        
+    step = step + 1
